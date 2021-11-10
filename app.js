7ee620cbe57c207a05aabd2bf23db5141fbabef4
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-11-10 17:59:17 -0500

    Added Login
    Added Login

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
-//const port = 3000;
+const port = 3000;
+const middleware = require('./middleware')
 
-//const server = app.listen(port, () => console.log("Listening on port " + port));
+const server = app.listen(port, () => console.log("Listening on port " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.get("/", (req, res, next) => {
+const loginRoute = require('./routes/loginRoutes');
+
+app.use("/login", loginRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
 
     var payload = {
         pageTitle: "Home"
