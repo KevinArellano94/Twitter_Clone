8ce13803f2154dfc6bf1266ff62a614680b12878
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-11-10 16:26:57 -0500

    Added "engine"
    Added "engine"

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const port = 3000;
 
 const server = app.listen(port, () => console.log("Listening on port " + port));
 
-app.set("view engin", "pug");
+app.set("view engine", "pug");
 app.set("views", "views");
 
 app.get("/", (req, res, next) => {
-    res.status(200).send("200")
+    res.status(200).render("home")
 })
