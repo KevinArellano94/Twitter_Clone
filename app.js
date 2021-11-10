27c077c16d333ca8e1cc2d96b3670477589d9ddd
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-11-10 17:04:34 -0500

    Updated port
    Updated port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+//const port = 3000;
 
-const server = app.listen(port, () => console.log("Listening on port " + port));
+//const server = app.listen(port, () => console.log("Listening on port " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
